1762cc704551caed01566544210d501128c2a5c9
Eder Lima <lima.eder101@gmail.com>
2022-10-31 05:02:40 -0300

    feat: update tests

--- src/tests/index.spec.ts
+++ src/tests/index.spec.ts
@@ -1,5 +1,4 @@
-import createBox, { derive } from '../index'
-import type { Blackbox } from '../index'
+import { createBox } from '../index'
 
 describe('createBox()', () => {
   let innerState = {
@@ -19,24 +18,14 @@ describe('createBox()', () => {
     expect(box).toBeInstanceOf(Object)
   })
 
-  it('should be able to create a box with a primitive.', () => {
-    const box = createBox('foo')
-    expect(box).toBeInstanceOf(Object)
-  })
-
   describe('box.get()', () => {
     it("should return the box's inner state.", () => {
       const box = createBox(innerState)
       expect(box.get()).toEqual(innerState)
     })
-
-    it("should return the box's inner state when created with a primitive.", () => {
-      const box = createBox('foo')
-      expect(box.get()).toEqual('foo')
-    })
   })
 
-  describe('box.set()', () => {
+  describe('box.update()', () => {
     it('should mutate the internal state of the box.', () => {
       const box = createBox(innerState)
 
@@ -45,21 +34,12 @@ describe('createBox()', () => {
         country: 'Brazil',
       }
 
-      box.set((state) => {
+      box.update((state) => {
         state.city = expected.city
         state.country = expected.country
-        return state
       })
 
       expect(box.get()).toEqual(expected)
-    })
-
-    it('should mutate the internal state of the box when it is a primitive.', () => {
-      const box = createBox(0)
-
-      box.set((state) => state + 1)
-
-      expect(box.get()).toBe(1)
     })
   })
 
@@ -72,109 +52,58 @@ describe('createBox()', () => {
         country: 'Brazil',
       }
 
-      box.update(expected)
+      box.set(expected)
 
       expect(box.get()).toEqual(expected)
     })
+  })
 
-    it('should update the internal state of the box with the given primitive value', () => {
-      const box = createBox(10)
-      const expected = 20
+  describe('box.reset()', () => {
+    it('should reset the internal state of the box to the initial value.', () => {
+      const box = createBox(innerState)
 
-      box.update(expected)
-      expect(box.get()).toBe(expected)
+      const expected = {
+        city: 'Campinas',
+        country: 'Brazil',
+      }
+
+      box.set(expected)
+      box.reset()
+
+      expect(box.get()).toEqual(innerState)
     })
   })
 
   describe('box.subscribe()', () => {
-    it('should be able to subscribe to a box state update', (done) => {
-      const box = createBox(0)
-      const expected = 10
+    it('should trigger a callback when the state is updated', () => {
+      const box = createBox(innerState)
 
-      box.subscribe((state) => {
-        expect(state).toBe(expected)
-        done()
-      })
+      const expected = {
+        city: 'Campinas',
+        country: 'Brazil',
+      }
 
-      box.update(expected)
+      const callback = jest.fn()
+
+      box.subscribe(callback)
+
+      box.set(expected)
+
+      expect(callback).toHaveBeenCalledWith(expected)
     })
 
-    it('should trigger a callback when the ".update()" method is called', (done) => {
-      const box = createBox(0)
-      const expected = 10
+    it('should return a function to unsubscribe the callback', () => {
+      const box = createBox(innerState)
 
-      box.subscribe((state) => {
-        expect(state).toBe(10)
-        done()
-      })
+      const callback = jest.fn()
 
-      box.update(expected)
-    })
+      const unsubscribe = box.subscribe(callback)
 
-    it('should trigger a callback when the ".set()" method is called', (done) => {
-      const box = createBox(0)
-      const expected = 10
+      unsubscribe()
 
-      box.subscribe((state) => {
-        expect(state).toBe(10)
-        done()
-      })
+      box.set({ city: 'Campinas' })
 
-      box.set(() => expected)
-    })
-  })
-})
-
-describe('derive()', () => {
-  const state = {
-    city: 'São Paulo',
-    country: 'Brazil',
-  }
-
-  type State = typeof state
-
-  let box: Blackbox.BoxType<State>
-  const identity = <A>(value: A) => value
-
-  beforeEach(() => {
-    box = createBox(state)
-  })
-
-  it('should create a derived box.', () => {
-    const derived = derive(box, identity)
-
-    expect(derived).toBeInstanceOf(Object)
-  })
-
-  describe('derived.get()', () => {
-    let derived: Blackbox.ObservableBoxType<string>
-
-    beforeEach(() => {
-      derived = derive(box, (state) => state.city)
-    })
-
-    it('should get the internal state of the box.', () => {
-      const expected = state.city
-      expect(derived.get()).toBe(expected)
-    })
-  })
-
-  describe('derived.subscribe()', () => {
-    let derived: Blackbox.ObservableBoxType<string>
-
-    beforeEach(() => {
-      derived = derive(box, (state) => state.city)
-    })
-
-    it('should trigger a callback when the original box state is mutated.', (done) => {
-      const expected = 'Campinas'
-
-      derived.subscribe((city) => {
-        expect(city).toBe(expected)
-        done()
-      })
-
-      box.set((state) => ({ ...state, city: expected }))
+      expect(callback).not.toHaveBeenCalled()
     })
   })
 })
